99162b01f8f576e15557542d70dad7d5174d444e
anandprasad <anandreddy0020@gmail.com>
2022-08-02 17:18:38 +0530

    final project

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use('/',authRoutes);
 app.use('/post',postRoutes);
 
 const CONNECTION_URL = "mongodb://localhost:27017/tourist-guide"
+// const CONNECTION_URL = "mongodb+srv://miniproject:miniproject@cluster0.wbss9.mongodb.net/?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{useUnifiedTopology: true},{useNewUrlParser: true},{useFindAndModify:true})
