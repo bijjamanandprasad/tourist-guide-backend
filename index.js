7ca9e854a00e50ce948c4c4b6124348c648aabf1
anandprasad <anandreddy0020@gmail.com>
2022-08-04 12:15:15 +0530

    bcrypt is updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/post',postRoutes);
 // const CONNECTION_URL = "mongodb://localhost:27017/tourist-guide"
 const CONNECTION_URL = "mongodb+srv://miniproject:miniproject@tourist.wbss9.mongodb.net/?retryWrites=true&w=majority";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(CONNECTION_URL,{useUnifiedTopology: true},{useNewUrlParser: true},{useFindAndModify:true})
         .then(() => {
